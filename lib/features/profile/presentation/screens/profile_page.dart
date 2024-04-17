dca51ef0a4bb1ae2807763082d7654e25eff6b52
Raiymbek <yeslyamr@gmail.com>
2024-04-17 20:15:13 +0500

    removed unnecessary profile tiles

--- lib/features/profile/presentation/screens/profile_page.dart
+++ lib/features/profile/presentation/screens/profile_page.dart
@@ -2,7 +2,6 @@ import 'package:awesome_extensions/awesome_extensions.dart' hide NavigatorExt;
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get_it/get_it.dart';
-import 'package:go_router/go_router.dart';
 import 'package:synapso/core/utils/key_value_storage_service.dart';
 import 'package:synapso/features/authentication/models/user_model.dart';
 import 'package:synapso/features/authentication/stores/authentication_store.dart';
@@ -25,7 +24,7 @@ class _ProfilePageState extends State<ProfilePage> {
         title: const Text('Profile'),
       ),
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Text(
             '${userModel?.name ?? ''} ${userModel?.surname ?? ''}',
@@ -43,48 +42,48 @@ class _ProfilePageState extends State<ProfilePage> {
               fontWeight: FontWeight.w400,
             ),
           ).paddingOnly(bottom: 32.h).toCenter(),
-          Text(
-            'Settings',
-            style: TextStyle(
-              fontSize: 20.sp,
-              fontFamily: 'Inter',
-              fontWeight: FontWeight.w500,
-            ),
-          ).paddingOnly(bottom: 24.h),
-          ListTile(
-            onTap: () => context.pushNamed('personal_data'),
-            contentPadding: EdgeInsets.zero,
-            title: Text(
-              'Personal data',
-              style: TextStyle(
-                fontSize: 18.sp,
-                fontFamily: 'Inter',
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-            trailing: ImageIcon(
-              const AssetImage('assets/icons/arrow_forward.png'),
-              color: const Color(0xFF9D9D9D),
-              size: 32.h,
-            ),
-          ),
-          ListTile(
-            onTap: () => context.pushNamed('change_password'),
-            contentPadding: EdgeInsets.zero,
-            title: Text(
-              'Change password',
-              style: TextStyle(
-                fontSize: 18.sp,
-                fontFamily: 'Inter',
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-            trailing: ImageIcon(
-              const AssetImage('assets/icons/arrow_forward.png'),
-              color: const Color(0xFF9D9D9D),
-              size: 32.h,
-            ),
-          ).paddingOnly(bottom: 12.h),
+          // Text(
+          //   'Settings',
+          //   style: TextStyle(
+          //     fontSize: 20.sp,
+          //     fontFamily: 'Inter',
+          //     fontWeight: FontWeight.w500,
+          //   ),
+          // ).paddingOnly(bottom: 24.h),
+          // ListTile(
+          //   onTap: () => context.pushNamed('personal_data'),
+          //   contentPadding: EdgeInsets.zero,
+          //   title: Text(
+          //     'Personal data',
+          //     style: TextStyle(
+          //       fontSize: 18.sp,
+          //       fontFamily: 'Inter',
+          //       fontWeight: FontWeight.w500,
+          //     ),
+          //   ),
+          //   trailing: ImageIcon(
+          //     const AssetImage('assets/icons/arrow_forward.png'),
+          //     color: const Color(0xFF9D9D9D),
+          //     size: 32.h,
+          //   ),
+          // ),
+          // ListTile(
+          //   onTap: () => context.pushNamed('change_password'),
+          //   contentPadding: EdgeInsets.zero,
+          //   title: Text(
+          //     'Change password',
+          //     style: TextStyle(
+          //       fontSize: 18.sp,
+          //       fontFamily: 'Inter',
+          //       fontWeight: FontWeight.w500,
+          //     ),
+          //   ),
+          //   trailing: ImageIcon(
+          //     const AssetImage('assets/icons/arrow_forward.png'),
+          //     color: const Color(0xFF9D9D9D),
+          //     size: 32.h,
+          //   ),
+          // ).paddingOnly(bottom: 12.h),
           TextButton(
             onPressed: () async {
               await GetIt.I<AuthenticationStore>().logOut();
